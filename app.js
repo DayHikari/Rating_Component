79488aad1fef15bf7e5bfd6404db38d8f7db0b4f
David Mason <DIMason1702@gmail.com>
2023-10-15 17:32:54 +0100

    JS and thank you page started

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Hello, world!");
+//  Variables
+// Star buttons
+const starButtons = document.querySelectorAll(".star-button");
+
+// Submit button
+const submitButton = document.querySelector("#submit");
+
+// Submit buttons a tag
+const submitLink = document.querySelector("#submit-link")
+
+// Selected score variable
+let score = "";
+
+
+//  Event listeners
+// Star button click event listener
+starButtons.forEach(element => {
+    element.addEventListener("click", function () {
+        starButtons.forEach(all => {
+            all.className = "star-button";
+        })
+        element.className = "selected-star-button"
+        score = element.textContent;
+        submitLink.setAttribute("href", "thank-you.html");
+    })
+});
+
+// Submit button click event listener
+submitButton.addEventListener("click", function () {
+    if (score === "") {
+        alert("Please select a score")
+    }
+})
